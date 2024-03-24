ecc765d825960bb9eb1cf75d831d2f2c30fd7a05
Marshall FRY <marshalfry1998@gmail.com>
2024-03-24 01:13:42 +0100

    fix

--- api/src/config/sequelize.ts
+++ api/src/config/sequelize.ts
@@ -1,6 +1,12 @@
 import { Sequelize } from "sequelize";
 import "dotenv/config";
 
+console.log("process.env.DATABASE_NAME", process.env.DATABASE_NAME)
+console.log("process.env.DATABASE_USERNAME", process.env.DATABASE_USERNAME)
+console.log("process.env.DATABASE_PASSWORD", process.env.DATABASE_PASSWORD)
+console.log("process.env.DATABASE_HOST", process.env.DATABASE_HOST)
+console.log("process.env.MYSQLDB_DOCKER_PORT", process.env.MYSQLDB_DOCKER_PORT)
+
 const sequelize = new Sequelize(
     process.env.DATABASE_NAME as string, 
     process.env.DATABASE_USERNAME as string, 
